d5b46fb4e563d6c529cbf2be476c3235e99b3bb7
Marcos Morais <marcostmorais@gmail.com>
2019-06-08 12:08:34 -0300

    AppKit Core Files updated to suit project needs

--- SKSmoothCameraNode/Core/GameViewController.swift
+++ SKSmoothCameraNode/Core/GameViewController.swift
@@ -16,19 +16,15 @@ class GameViewController: UIViewController {
         super.viewDidLoad()
         
         if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
+
+            let scene = GameScene(size: self.view.bounds.size)
+            scene.scaleMode = .aspectFill
             
+            view.presentScene(scene)
             view.ignoresSiblingOrder = true
-            
             view.showsFPS = true
             view.showsNodeCount = true
+            
         }
     }
 
