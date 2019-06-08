c42449f16680d9c86b57c490d886035eb82cbca8
Marcos Morais <marcostmorais@gmail.com>
2019-06-08 19:41:41 -0300

    Updated all declarations for SKSmoothCameraNode

--- Example/SKSmoothCameraNode/SKSmoothCameraNode.swift
+++ Example/SKSmoothCameraNode/SKSmoothCameraNode.swift
@@ -8,11 +8,50 @@
 
 import SpriteKit
 
+/**
+ An object that subclasses SKCameraNode and adds smoothing to its core.
+ - SeeAlso: SKCameraNode
+ */
 class SKSmoothCameraNode: SKCameraNode {
     
-    let cameraAlpha: CGFloat = 0.050
-    var smoothCameraPosition: CGPoint = CGPoint.zero
+    /** This attribute determines the delay in which the smooth camera is going to be oriented.
+        It is highly recommended that this value stays in between 2.0 and 0.1.
+     */
+    var cameraAlpha: CGFloat = 0.7 {
+        didSet {
+            self.cameraAlpha = self.cameraAlpha/10
+        }
+    }
     
+    /** This position is the one to be normalized and updated into the position of the target node.
+     */
+    private var smoothCameraPosition: CGPoint = CGPoint.zero
+    
+    /**
+     
+     Creates a camera with the smoothing effect. It takes a camera alpha that acts as a multiplier for the delay.
+     - parameter cameraAlpha: Multiplier in which the camera is going to be oriented. The default value is 0.7.
+     - Returns: None.
+     - Requires: iOS 9.0 or above
+     */
+    init(cameraAlpha: CGFloat = 0.7) {
+        
+        super.init()
+        
+        defer {
+            self.cameraAlpha = cameraAlpha
+        }
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    /**
+     Method that updates the camera with the desired position related to the Scene.
+     - parameter position: the position of the target node in which the camera is going to be updated.
+     */
     func setCamera(position: CGPoint) {
         
         let actualPosition = position
