d570ae3818284fdad653e89a37f99b6b11db353b
Marcos Morais <marcostmorais@gmail.com>
2019-06-08 12:43:39 -0300

    Added +Touches extension to main scene

--- SKSmoothCameraNode/Scene Extensions/GameScene+Touches.swift
+++ SKSmoothCameraNode/Scene Extensions/GameScene+Touches.swift
@@ -6,4 +6,37 @@
 //  Copyright © 2019 Bit Journey Labs. All rights reserved.
 //
 
-import Foundation
+import SpriteKit
+
+extension GameScene {
+    
+    // MARK: Touches Methods
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.location(in: self)
+        self.player.position = touchLocation
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        guard let touch = touches.first else {
+            return
+        }
+        
+        let touchLocation = touch.location(in: self)
+        self.player.position = touchLocation
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    }
+    
+}
